8e5e34e4ee5448faecf02b159f9410938b86d60f
Lukas Hecke <Lukas.Hecke@meierhofer.com>
2021-02-12 14:14:50 +0100

    Add project files.

---
+++ TouchManipulation/TouchManipulation/TouchTracking/TouchEffect.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TouchManipulation.TouchTracking
+{
+    // Benötigten Klassen für die Touch Event Abhandlung mit Xamarin.Forms (SkiaSharp)
+    
+    public class TouchEffect : RoutingEffect
+    {
+        public event TouchActionEventHandler TouchAction;
+
+        public TouchEffect() : base("XamarinDocs.TouchEffect")
+        {
+        }
+
+        public bool Capture { set; get; }
+
+        /// <summary>
+        /// Muss nativ gefeuert werden!
+        /// </summary>
+        /// <param name="element"></param>
+        /// <param name="args"></param>
+        public void OnTouchAction(Element element, TouchActionEventArgs args)
+        {
+            TouchAction?.Invoke(element, args);
+        }
+    }
+
+    public delegate void TouchActionEventHandler(object sender, TouchActionEventArgs args);
+
+    public class TouchActionEventArgs : EventArgs
+    {
+        public TouchActionEventArgs(long id, TouchActionType type, Point location, bool isInContact)
+        {
+            Id = id;
+            Type = type;
+            Location = location;
+            IsInContact = isInContact;
+        }
+
+        public long Id { private set; get; }
+
+        public TouchActionType Type { private set; get; }
+
+        public Point Location { private set; get; }
+
+        public bool IsInContact { private set; get; }
+    }
+
+    public enum TouchActionType
+    {
+        Entered,
+        Pressed,
+        Moved,
+        Released,
+        Exited,
+        Cancelled
+    }
+}
---
+++ TouchManipulation/TouchManipulation/Views/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using TouchManipulation.TouchTracking;
+using TouchManipulation.ViewModels;
+using Xamarin.Forms;
+
+namespace TouchManipulation.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        SKBitmap resourceBitmap;
+
+        public MainPage()
+        {
+            LoadResourceBitmap();
+
+            InitializeComponent();
+            this.BindingContext = new MainPageViewModel();
+        }
+
+        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            canvas.DrawBitmap(resourceBitmap, new SKRect(0, info.Height / 3, info.Width, 2 * info.Height / 3));
+        }
+
+        private void OnTouchEffectAction(object sender, TouchActionEventArgs args)
+        {
+            // TODO LHE:
+            Console.WriteLine("OnTouchEffectAction");
+        }
+
+        private void LoadResourceBitmap()
+        {
+            string resourceID = "TouchManipulation.Media.octocat-jedi.jpg";
+            Assembly assembly = GetType().GetTypeInfo().Assembly;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceID))
+            {
+                resourceBitmap = SKBitmap.Decode(stream);
+            }
+        }
+    }
+}
