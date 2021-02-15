6d1d31dc08dfa948f1baa331f43dd25bd84381a1
Lukas Hecke <Lukas.Hecke@meierhofer.com>
2021-02-15 09:39:47 +0100

    Logging touch actions, removing unneccesary components

--- TouchManipulation/TouchManipulation/ViewModels/MainPageViewModel.cs
+++ TouchManipulation/TouchManipulation/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TouchManipulation.ViewModels
+﻿namespace TouchManipulation.ViewModels
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : ViewModelBase
     {
+        private string touchLog = "Hier werden alle Berührungsgesten geloggt.";
+
+        public string TouchLog
+        {
+            get
+            {
+                return this.touchLog;
+            }
+            set
+            {
+                this.touchLog = value;
+                this.OnPropertyChanged();
+            }
+        }
     }
 }
--- TouchManipulation/TouchManipulation/Views/MainPage.xaml.cs
+++ TouchManipulation/TouchManipulation/Views/MainPage.xaml.cs
@@ -1,42 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using SkiaSharp;
-using SkiaSharp.Views.Forms;
 using TouchManipulation.TouchTracking;
 using TouchManipulation.ViewModels;
 using Xamarin.Forms;
 
 namespace TouchManipulation.Views
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage
     {
-        SKBitmap resourceBitmap;
-
         public MainPage()
         {
             InitializeComponent();
             this.BindingContext = new MainPageViewModel();
         }
 
-        private void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
-        {
-            SKImageInfo info = args.Info;
-            SKSurface surface = args.Surface;
-            SKCanvas canvas = surface.Canvas;
-
-            canvas.Clear();
-        }
-
         private void OnTouchEffectAction(object sender, TouchActionEventArgs args)
         {
             // TODO LHE: https://docs.microsoft.com/de-de/xamarin/xamarin-forms/user-interface/graphics/skiasharp/transforms/touch#pinching-and-scaling
             Console.WriteLine("OnTouchEffectAction");
+            Log(args.Type.ToString());
+            Log(args.Location.ToString());
+        }
+
+        private void Log(string text)
+        {
+            ((MainPageViewModel)this.BindingContext).TouchLog += Environment.NewLine + text;
+            ScrollToEnd();
+        }
+
+        private void ScrollToEnd()
+        {
+            if (PART_ScrollView.ContentSize.Height > PART_ScrollView.Height)
+            {
+                double spaceBetweenLinesOnALabel = 5;
+                double scrollPositionY = PART_ScrollView.ContentSize.Height - PART_ScrollView.Height +
+                                         (((Label)PART_ScrollView.Content).FontSize*2) + spaceBetweenLinesOnALabel;
+                PART_ScrollView.ScrollToAsync(0, scrollPositionY, true);
+            }
         }
     }
 }
